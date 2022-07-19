8ae60be46a4f1e823b69851dc2347be383e56393
Rodrigo Barbosa Ponte <rodrigobponte@gmail.com>
2022-07-19 15:27:58 -0300

    update

--- scripts.js
+++ scripts.js
@@ -7,22 +7,29 @@ const html = document.querySelector('html')
 const body = document.querySelector('body')
 
 function openMenu() {
+  if (scrollY >= 100) {
+    header.classList.add('t-none')
+  }
   header.classList.add('menu-opened')
   open_Menu.classList.add('menu-opened')
   menu.classList.add('menu-opened')
   close_Menu.classList.remove('menu-closed')
   html.classList.add('overflow-hidden')
   body.classList.add('overflow-hidden')
-  
 }
 
 function closeMenu() {
-  header.classList.remove('menu-opened')
+  if (scrollY >= 100) {
+    header.classList.add('t-none')
+  } else {
+    header.classList.remove('menu-opened')
+  }
   open_Menu.classList.remove('menu-opened')
   menu.classList.remove('menu-opened')
   close_Menu.classList.add('menu-closed')
   html.classList.remove('overflow-hidden')
   body.classList.remove('overflow-hidden')
+
 }
 document.addEventListener('scroll', changeHeader)
 document.addEventListener('scroll', changeHeaderBack)
